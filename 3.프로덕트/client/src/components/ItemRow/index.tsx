bfdd6302edb9ac89ed609ad44b386024bc877803
Choi siun <siun0331@gmail.com>
2023-04-26 16:21:38 +0900

    refactor: 상세 페이지 아이템 세분화

---
+++ 3.프로덕트/client/src/components/ItemRow/index.tsx
@@ -0,0 +1,38 @@
+interface ItemRowProps {
+    itemName: string,
+    itemCategory: string,
+    mainAddress: string,
+    subAddress: string,
+    date: string,
+}
+
+const ItemRow = (props: ItemRowProps) => {
+    return(
+        <tr>
+            <th>
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                </label>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div>
+                        <div className="font-bold">{props.itemName}</div>
+                        <div className="text-sm opacity-50">{props.itemCategory}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {props.mainAddress}
+                <br/>
+                <span className="badge badge-ghost badge-sm">{props.subAddress}</span>
+            </td>
+            <td>{props.date}</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
+    )
+}
+
+export default ItemRow
