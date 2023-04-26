6858985c1e9d2dd8c05efe91e33e6c233df9aa51
Choi siun <siun0331@gmail.com>
2023-04-26 16:06:55 +0900

    feat: 메인 화면에 버튼 배치

--- 3.프로덕트/client/src/pages/index.tsx
+++ 3.프로덕트/client/src/pages/index.tsx
@@ -10,6 +10,19 @@ const MainPage = () => {
             </ContentCard>
         </div>
 
+        <div className={"flex w-full h-[180px] mt-6"}>
+            <div className={"w-full h-full mr-6"}>
+                <ContentCard cardName={'상품 확인하기'} >
+                    오늘의 배송 상품
+                </ContentCard>
+            </div>
+            <div className={"w-full h-full"}>
+                <ContentCard cardName={'지도 바로가기'} >
+                    지도 바로가기
+                </ContentCard>
+            </div>
+        </div>
+
 
     </>
   );
