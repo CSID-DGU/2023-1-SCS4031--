bbdf57b174ff50b8efb53c81e10ce10cbb653ac5
Choi siun <siun0331@gmail.com>
2023-06-13 23:53:20 +0900

    style: 배경색 최종 반영

--- 3.프로덕트/client/src/components/ContentCard/index.tsx
+++ 3.프로덕트/client/src/components/ContentCard/index.tsx
@@ -14,7 +14,7 @@ const ContentCard = ({ cardName, cardType, children }: ContentCardProps) => {
     if (cardType === "TODAY_MAP") {
       setImageUrl("primary");
     } else {
-      setImageUrl(`[url('/Image/delivery.png')]`);
+      setImageUrl(`accent`);
     }
   }, [imageUrl]);
 
