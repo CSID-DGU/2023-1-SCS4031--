dd6825292cb7b71126ee158ccf81f3de75c76985
Choi siun <siun0331@gmail.com>
2023-06-13 23:42:41 +0900

    chore: baseUrl setting

--- 3.프로덕트/client/vite.config.ts
+++ 3.프로덕트/client/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import react from '@vitejs/plugin-react'
-import * as path from "path";
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   // process에서 타입에러가 뜨게된다.
   // @type/node를 설치해주자
   return {
-    base: './',
+    base: "/",
     plugins: [
       react(),
       createHtmlPlugin({
@@ -20,14 +19,5 @@ export default ({ mode }) => {
         }
       }),
     ],
-    resolve: {
-      alias: [
-        { find: "@", replacement: path.resolve(__dirname, "src") },
-        {
-          find: "@public",
-          replacement: path.resolve(__dirname, "public"),
-        },
-      ],
-    }
   }
 }
